2d0e265c246010c0f72f55e336a3f36b8014d60c
aiworkslab <aiworkslab95@gmail.com>
2026-06-10 22:21:19 +0900

    Add multiple PDF selection support / 複数PDF選択機能追加

--- src/main.py
+++ src/main.py
@@ -10,52 +10,60 @@ root = tk.Tk()
 root.withdraw()
 
 # PDF選択
-pdf_file = filedialog.askopenfilename(
+# pdf_file = filedialog.askopenfilename(
+#     title="PDFファイルを選択してください",
+#     filetypes=[("PDFファイル", "*.pdf")]
+# )
+
+# PDF選択
+pdf_files = filedialog.askopenfilenames(
     title="PDFファイルを選択してください",
     filetypes=[("PDFファイル", "*.pdf")]
 )
 
 # 結果表示
-if pdf_file:
-    print("選択したファイル:")
-    print(pdf_file)
+if pdf_files:
     
-    file_name = os.path.basename(pdf_file)
+    for pdf_file in pdf_files:
+        print("選択したファイル:")
+        print(pdf_file)
+        
+        file_name = os.path.basename(pdf_file)
 
-    today = datetime.now().strftime("%Y%m%d")
-    new_file_name = today + "_" + file_name
-    
-    pdf_path = Path(pdf_file)
-    new_file_path = pdf_path.parent / new_file_name
+        today = datetime.now().strftime("%Y%m%d")
+        new_file_name = today + "_" + file_name
+        
+        pdf_path = Path(pdf_file)
+        new_file_path = pdf_path.parent / new_file_name
 
-    print("\n変更前ファイル名:")
-    print(file_name)
+        print("\n変更前ファイル名:")
+        print(file_name)
 
-    print("\n変更後ファイル名:")
-    print(new_file_name)
+        print("\n変更後ファイル名:")
+        print(new_file_name)
 
-    if new_file_path.exists():
-        messagebox.showerror(
-            "エラー",
-            "同じ名前のファイルが存在します。"
-        )
-        print("同じ名前のファイルが存在します。")
-    else:
-
-        answer = messagebox.askyesno(
-            "確認",
-            f"この名前でリネームしますか？\n\n{new_file_name}"
-        )
-
-        if answer:
-            pdf_path.rename(new_file_path)
-            print("リネームしました。")
-            messagebox.showinfo("完了",
-                                "リネーム完了")
+        if new_file_path.exists():
+            messagebox.showerror(
+                "エラー",
+                "同じ名前のファイルが存在します。"
+            )
+            print("同じ名前のファイルが存在します。")
         else:
-            print("リネームをキャンセルしました。")
-            messagebox.showinfo("キャンセル",
-                    "リネームをキャンセルしました。")
+
+            answer = messagebox.askyesno(
+                "確認",
+                f"この名前でリネームしますか？\n\n{new_file_name}"
+            )
+
+            if answer:
+                pdf_path.rename(new_file_path)
+                print("リネームしました。")
+                messagebox.showinfo("完了",
+                                    "リネーム完了")
+            else:
+                print("リネームをキャンセルしました。")
+                messagebox.showinfo("キャンセル",
+                        "リネームをキャンセルしました。")
 
 else:
     print("PDFが選択されませんでした。")
